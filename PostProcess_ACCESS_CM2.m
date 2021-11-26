82ab0193dcfcb7864661d450b0e1128c9a415b90
Ryan Holmes <holmes.ryan.m@gmail.com>
2021-11-26 16:13:09 +1100

    Streamline post-process and remove unneeded functions.

--- PostProcess_ACCESS_CM2.m
+++ PostProcess_ACCESS_CM2.m
@@ -4,13 +4,7 @@
 
 %%%%%% OPTIONS %%%
 clear all;
-% $$$ baseMAT = '/srv/ccrc/data03/z3500785/access-cm2/';
 baseMAT = 'D:/DATA/access-cm2/';
-
-PI_or_his = 1;
-% 1 = PI-control, 0 = historical simualtion,
-% 2 = hisNAT e1, 3 = hisNAT e2, 4 = hisNAT e3
-% 5 = hisAER e1, 6 = hisGHG e1
 
 % Streamline post-processing:
 doBUDGET = 1;
@@ -18,30 +12,8 @@ doTyz = 0;
 doMOCyz = 0;
 doTAU = 0;
 
-if (PI_or_his == 1)
-    load([baseMAT 'CM2_PIcontrolTb05__ALL.mat']);
-    saveNAME = 'PIcontrolTb05PP_Tint.mat';
-% $$$     load([baseMAT 'CM2_PIcontrolTb05_ypm60_ALL.mat']);
-% $$$     saveNAME = 'PIcontrolTb05PP_ypm60_Tint.mat';
-% $$$ elseif (PI_or_his == 0)
-% $$$     load([baseMAT 'CM2_hisTb05__ALL.mat']);
-% $$$     saveNAME = 'hisPP_Tb05.mat';
-% $$$ elseif (PI_or_his == 2)
-% $$$     load([baseMAT 'CM2_hisNATe1Tb05__ALL.mat']);
-% $$$     saveNAME = 'hisNATe1PP_Tb05.mat';
-% $$$ elseif (PI_or_his == 3)
-% $$$     load([baseMAT 'CM2_hisNATe2__ALL.mat']);
-% $$$     saveNAME = 'hisNATe2PP.mat';
-% $$$ elseif (PI_or_his == 4)
-% $$$     load([baseMAT 'CM2_hisNATe3__ALL.mat']);
-% $$$     saveNAME = 'hisNATe3PP.mat';
-% $$$ elseif (PI_or_his == 5)
-% $$$     load([baseMAT 'CM2_hisAERe1__ALL.mat']);
-% $$$     saveNAME = 'hisAERe1PP.mat';
-% $$$ elseif (PI_or_his == 6)
-% $$$     load([baseMAT 'CM2_hisGHGe1__ALL.mat']);
-% $$$     saveNAME = 'hisGHGe1PP.mat';
-end
+load([baseMAT 'CM2_PIcontrolTb05__ALL.mat']);
+saveNAME = 'PIcontrolTb05PP_Tint.mat';
 
 saveMAT = 1;
 
@@ -56,50 +28,12 @@ P = (Pe(2:end)+Pe(1:end-1))/2;
 PL = length(P);
 dP = repmat(dP,[PL 1]);
 
-% $$$ % Non-linear using mean T:
-% $$$ Vmean = cat(1,cumsum(mean(Tv.V,2),1,'reverse'),0);
-% $$$ Pe = unique((Vmean/Vmean(1))*100);
-% $$$ % $$$ % double number of points:
-% $$$ % $$$ PeN = zeros(length(Pe)*2-1,1);
-% $$$ % $$$ PeN(1) = Pe(1);PeN(end) = Pe(end);
-% $$$ % $$$ cnt = 1;
-% $$$ % $$$ for ii = 1:(length(Pe)-1)
-% $$$ % $$$     PeN(cnt+1) = (Pe(ii) + Pe(ii+1))/2;
-% $$$ % $$$     PeN(cnt+2) = Pe(ii+1);
-% $$$ % $$$     cnt = cnt+2;
-% $$$ % $$$ end
-% $$$ % $$$ Pe = PeN;
-% $$$ dP = diff(Pe);
-% $$$ P = (Pe(2:end)+Pe(1:end-1))/2;
-% $$$ PL = length(P);
-
 % Time vector:
 tL = length(time);
 time = time/365.25; % time in years
 
-% $$$ % Coarse grain temperature bins (assumes linear):
-% $$$ n = 1; % Coarse grain factor
-% $$$ TLn = floor(TL/n);
-% $$$ TLr = mod(TL,n);
-% $$$ convm = zeros(TL,TLn);
-% $$$ cnt = 1;
-% $$$ for i=1:TLn
-% $$$     convm(cnt:(cnt+n-1),i) = 1;
-% $$$     cnt = cnt+n;
-% $$$ end
-% $$$ if (TLr>0)
-% $$$     convm((end-TLr+1):end,end) = 1;
-% $$$ end
-% $$$ dT = n*dT;
-% $$$ Te = -3:dT:(-3+TLn*dT);
-% $$$ T = (Te(2:end)+Te(1:end-1))/2;
-% $$$ TL = length(T);
-% $$$ fns = fieldnames(Tv);
-% $$$ for vi=1:length(fns)
-% $$$     eval(['Tv.' fns{vi} ' = (Tv.' fns{vi} '''*convm)'';']);
-% $$$ end
-
-% Fix advection budget term by residual:
+% The temp_advection term in the ACCESS-CM2 output is
+% corrupted. Fix this by calculating it by residual:
 typs = {'T','Z','Y'};
 vars = {'temp_submeso', 'temp_vdiffuse_diff_cbt', 'temp_nonlocal_KPP', ...
         'temp_vdiffuse_sbc','frazil_3d','sw_heat','temp_rivermix', ...
@@ -130,8 +64,6 @@ for vi = 1:length(typs)
     eval([typs{vi} 'v.RMIX = ' typs{vi} 'v.neutral_diffusion_temp+' typs{vi} 'v.temp_vdiffuse_k33+' typs{vi} 'v.mixdownslope_temp' ...
           '+' typs{vi} 'v.temp_sigma_diff;']);
     eval([typs{vi} 'v.VMIX = ' typs{vi} 'v.temp_vdiffuse_diff_cbt+' typs{vi} 'v.temp_nonlocal_KPP;']);
-% $$$     eval([typs{vi} 'v.ADV = ' typs{vi} 'v.TEN-' typs{vi} 'v.FOR-' ...
-% $$$           typs{vi} 'v.RMIX-' typs{vi} 'v.VMIX;']); - Machine noise error!! Zv is 4 / 2.5e15, Tv is 0.125 / 1.27e14
 end
 vars = {vars{:},'temp_tendency','temp_advection'};
 
@@ -172,28 +104,7 @@ if (doBUDGET)
     Tv = rmfield(Tv,'pme_river_c');Zv = rmfield(Zv,'pme_river_c');Yv = rmfield(Yv,'pme_river_c');
 end
 
-% $$$ % Check temperature budget:
-% $$$ figure;
-% $$$ plot(Te,mean(Tv.TEN_c,2),'-m');
-% $$$ hold on;
-% $$$ plot(Te,mean(Tv.FOR_c,2),'-k');
-% $$$ plot(Te,mean(Tv.ADV_c,2),'-b');
-% $$$ plot(Te,mean(Tv.RMIX_c,2),'-g');
-% $$$ plot(Te,mean(Tv.VMIX_c,2),'-r');
-% $$$ plot(Te,mean(Tv.JSH_c,2),'-c');
-% $$$ 
-
 OHC = squeeze(Zv.H_c(end,:))';
-
-% $$$ % Time-integrate budget terms (prior to remapping to P):
-% $$$ bvars = {'TEN_c','ADV_c','FOR_c','RMIX_c','VMIX_c'};
-% $$$ for ti =1:length(typs)
-% $$$     for vi=1:length(bvars)
-% $$$         eval([typs{ti} 'v.' bvars{vi} ' = cumsum(' typs{ti} ...
-% $$$               'v.' bvars{vi} '.*repmat(DT_A'',[length(' typs{ti} ...
-% $$$               'v.' bvars{vi} '(:,1)) 1]),2);']);
-% $$$     end
-% $$$ end    
 
 
 %%% Remap to percentile
@@ -206,62 +117,7 @@ Zv.P   = 100*Zv.V_c./repmat(Zv.V_c(end,:),[zL+1 1]);
 Yv.P   = 100*Yv.V_c./repmat(Yv.V_c(end,:),[yL+1 1]);
 Tv.Pa  = 100*Tv.A_c./repmat(Atot',[TL+1 1]);
 
-NEWold = 0;
-
-% Binning issue notes:
-% - Binning H rather than T is worse in temperature, but similar in
-%   depth and latitude. This is with linear %-bins. However, with
-%   non-linear %-bins based exactly on the mean T volume profile H is
-%   only a bit worse than T - I guess this is consistent with
-%   actually having almost exactly the right volume in each %-bin?
-% - It doesn't make sense to pick %-bins non-linear based on T 
-%   for Z and latitude?
-% - 0.05C temperature bins helps.
-% - Need high-res %-bins to resolve very warm temperatures.
-% - Part of the problem is that there are large percentile jumps for a
-%   given temperature jump. E.g. with 0.2C bins it's jumping up to 8\% per
-%   temperature bin.
-% Conclusion: For simplicity and because of the lack of an obvious
-% improvement, I'm sticking with 0.05C temperature bins and 0.25%
-% percentile bins.
-
-
-if (NEWold)
-
-    %%% New approach: Interpolate HC
-    % Interpolate onto high-resolution grids for smoothness:
-% $$$     zfac = 1;
-% $$$     Zv.H_c = interp1(Zv.H_c,1:1/zfac:(zL+1));Zv.V_c = interp1(Zv.V_c,1:1/zfac:(zL+1));
-% $$$     Tfac = 1;
-% $$$     Tv.H_c = interp1(Tv.H_c,1:1/Tfac:(TL+1));Tv.V_c = interp1(Tv.V_c,1:1/Tfac:(TL+1));
-% $$$     yfac = 1;
-% $$$     Yv.H_c = interp1(Yv.H_c,1:1/yfac:(yL+1));Yv.V_c = interp1(Yv.V_c,1:1/yfac:(yL+1));
-
-    for ti = 1:tL
-        [Pun,Iun] = unique(Zv.P(:,ti));
-        ZvP.Hp(:,ti) = interp1(Pun,Zv.H_c(Iun,ti),Pe,'linear');
-        [Pun,Iun] = unique(Tv.P(:,ti));
-        TvP.Hp(:,ti) = interp1(Pun,Tv.H_c(Iun,ti),Pe,'linear');
-        [Pun,Iun] = unique(Yv.P(:,ti));
-        YvP.Hp(:,ti) = interp1(Pun,Yv.H_c(Iun,ti),Pe,'linear');
-        [Pun,Iun] = unique(Tv.Pa(:,ti));
-        TvP.Hap(:,ti) = interp1(Pun,Tv.A_c(Iun,ti),Pe,'linear');
-    end
-    % Note: No need to set 0 or maximum with above "unique" approach
-
-    % Calculate temperatures from HC:
-    ZvP.Tp = diff(ZvP.Hp,[],1)/rho0/Cp*100./repmat(dP,[1 tL])./repmat(Zv.V_c(end,:),[PL 1]);
-    YvP.Tp = diff(YvP.Hp,[],1)/rho0/Cp*100./repmat(dP,[1 tL])./repmat(Yv.V_c(end,:),[PL 1]);
-    TvP.Tp = diff(TvP.Hp,[],1)/rho0/Cp*100./repmat(dP,[1 tL])./repmat(Tv.V_c(1,:),[PL 1]);
-    TvP.Tap = diff(TvP.Hap,[],1)/rho0/Cp*100./repmat(dP,[1 tL])./repmat(Atot',[PL 1]);
-
-    % Note: I think this above is the problem - when you divide by
-    % the assumed proportional to Delta P volume here this is not
-    % consistent with the actual volume in the bin (given the
-    % linear interpolation).
-    
-else
-    %%% Old approach: Interpolate temperatures
+%%% Interpolate temperatures
     % Calculate z-temperature:
     Zv.T = Zv.H/rho0/Cp./Zv.V;
     Zv.Te = cat(1,Zv.T(1,:),(Zv.T(2:end,:)+Zv.T(1:end-1,:))/2,Zv.T(end,:));
@@ -271,18 +127,12 @@ else
     Yv.Te = cat(1,Yv.T(1,:),(Yv.T(2:end,:)+Yv.T(1:end-1,:))/2,Yv.T(end,:));
 
     % Interpolate variables to P levels:
-% $$$     TvP.Tp = zeros(PL,tL);
-% $$$     TvP.Tap = zeros(PL,tL);
-% $$$     ZvP.Tp = zeros(PL,tL);
-% $$$     YvP.Tp = zeros(PL,tL);
     for ti = 1:tL
         [Pun,Iun] = unique(Tv.P(:,ti));
         TvP.Tp(:,ti) = interp1(Pun,Te(Iun),P,'linear');
-% $$$         TvP.Tp(1,ti) = Te(end);
         
         [Pun,Iun] = unique(Tv.Pa(:,ti));
         TvP.Tap(:,ti) = interp1(Pun,Te(Iun),P,'linear');
-% $$$         TvP.Tap(1,ti) = Te(end);
         
         [Pun,Iun] = unique(Zv.P(:,ti));
         ZvP.Tp(:,ti) = interp1(Pun,Zv.Te(Iun,ti),P,'linear');
@@ -292,7 +142,6 @@ else
         
         [Pun,Iun] = unique(Tv.P(:,ti));
         TvP.A_c(:,ti) =  interp1(Pun,Tv.A_c(Iun,ti),Pe,'linear');
-% $$$         TvP.A_c(end,ti) = Atot(ti);
     end
     TvP.A = diff(TvP.A_c,[],1);
 end
@@ -307,9 +156,6 @@ if (doBUDGET)
             eval(['YvP.' bvars{vi} '(:,ti) = interp1(Pun,Yv.' bvars{vi} '(Iun,ti),Pe,''linear'');']);
             [Pun,Iun] = unique(Tv.P(:,ti));
             eval(['TvP.' bvars{vi} '(:,ti) = interp1(Pun,Tv.' bvars{vi} '(Iun,ti),Pe,''linear'');']);
-% $$$             eval(['ZvP.' bvars{vi} '(1,ti) = 0;']);eval(['ZvP.' bvars{vi} '(end,ti) = Zv.' bvars{vi} '(end,ti);']);
-% $$$             eval(['TvP.' bvars{vi} '(1,ti) = 0;']);eval(['TvP.' bvars{vi} '(end,ti) = Tv.' bvars{vi} '(1,ti);']);
-% $$$             eval(['YvP.' bvars{vi} '(1,ti) = 0;']);eval(['YvP.' bvars{vi} '(end,ti) = Yv.' bvars{vi} '(end,ti);']);
         end
     end
 
@@ -332,12 +178,7 @@ end
                                                     % go with depth-volume
     
     % Construct climatology and make years even:
-    if (PI_or_his == 1)
-        yr1 =50; % initial years to throw out
-% $$$         yr1 =0;
-    else
-        yr1 = 0;
-    end
+    yr1 =50; % initial years to throw out
     nyrs = floor(tL/12)-yr1; % number of years
     tL = nyrs*12; % number of months
     ti = yr1*12+1; % starting month
@@ -382,8 +223,7 @@ end
         ZvP.A = TvP.A;
         YvP.A = TvP.A; % for convience -> delete after...
     end
-    if (PI_or_his == 1) % Calculate cubic drift from PI-control
-        
+    % Calculate cubic drift from PI-control        
     for ti = 1:length(typs)
         for vi = 1:length(fnames)
             tn = typs{ti};
@@ -410,105 +250,6 @@ end
     end
     [OHC_cub,OHC_cubtr] = cubfit(time,OHC);
     OHC = OHC-OHC_cub;
-
-    else 
-
-        % Use PI-control cubic drift for de-drifting
-        
-        Itime = -100;
-        t = time-Itime;
-        t2 = (time-Itime).^2;
-        t3 = (time-Itime).^3;
-        
-        PIcont = load([baseMAT 'PIcontrolPP_Tb05.mat'],'ZvP','TvP','YvP','OHC_cubtr','time');
-        
-        for ti = 1:length(typs)
-            for vi = 1:length(fnames)
-                tn = typs{ti};
-                vn = fnames{vi};
-
-                % Mean (from PI control):
-                eval([tn 'vP.' vn '_mean = PIcont.' tn 'vP.' vn '_mean;']);
-            
-                % Calculate cubic trend:
-                eval(['lg = length(' tn 'vP.' vn '(:,1));']);
-                eval([tn 'vP.' vn '_cub = zeros(tL,lg);']);
-                for pi=1:lg
-                    eval([tn 'vP.' vn '_cub(:,pi) = PIcont.' ...
-                          tn 'vP.' vn '_cubtr(1,pi) + PIcont.' ...
-                          tn 'vP.' vn '_cubtr(2,pi).*t + PIcont.' ...
-                          tn 'vP.' vn '_cubtr(3,pi).*t2 + PIcont.' ...
-                          tn 'vP.' vn '_cubtr(4,pi).*t3;']);
-                end
-                eval([tn 'vP.' vn '_cub = ' tn 'vP.' vn '_cub'';']);
-            
-                % subtract cubic trend:
-                eval([tn 'vP.' vn ' = ' tn 'vP.' vn '-' tn 'vP.' vn '_cub;']);
-            
-            end
-        end
-        OHC_cub = PIcont.OHC_cubtr(1)+PIcont.OHC_cubtr(2).*t + ...
-                  PIcont.OHC_cubtr(3).*t2 + PIcont.OHC_cubtr(4).*t3;
-        OHC = OHC-OHC_cub;
-        OHCcubdd = OHC;
-        TIMESERIES = {TIMESERIES{:},'OHCcubdd'};
-
-        % Set date:
-        time = time+1850;
-        yr1 = 1850;
-        
-        % Dedrift with linear fit to historical e1:
-        if (PI_or_his == 2)
-            for ti = 1:length(typs)
-                for vi = 1:length(fnames)
-                    tn = typs{ti};
-                    vn = fnames{vi};
-
-                    % Calculate linear trend:
-                    eval(['lg = length(' tn 'vP.' vn '(:,1));']);
-                    eval([tn 'vP.' vn '_lin = zeros(tL,lg);']);
-                    eval([tn 'vP.' vn '_lintr = zeros(2,lg);']);
-                    for pi=1:lg
-                        eval(['[' tn 'vP.' vn '_lin(:,pi),' tn 'vP.' vn ...
-                              '_lintr(:,pi)] = linfit(time,' tn 'vP.' vn ...
-                              '(pi,:)'');']);
-                    end
-                    eval([tn 'vP.' vn '_lin = ' tn 'vP.' vn '_lin'';']);
-            
-                    % subtract linear trend:
-                    eval([tn 'vP.' vn ' = ' tn 'vP.' vn '-' tn 'vP.' vn '_lin;']);
-            
-                end
-            end
-            [OHC_lin,OHC_lintr] = linfit(time,OHC);
-            OHC = OHC-OHC_lin;
-        elseif (PI_or_his == 0)
-            hisNATe1 = load([baseMAT 'hisNATe1PP_Tb05.mat'],'ZvP','TvP','YvP','OHC_lintr','time');
-        
-            for ti = 1:length(typs)
-                for vi = 1:length(fnames)
-                    tn = typs{ti};
-                    vn = fnames{vi};
-
-                    % Calculate linear trend:
-                    eval(['lg = length(' tn 'vP.' vn '(:,1));']);
-                    eval([tn 'vP.' vn '_lin = zeros(tL,lg);']);
-                    for pi=1:lg
-                        eval([tn 'vP.' vn '_lin(:,pi) = hisNATe1.' ...
-                              tn 'vP.' vn '_lintr(1,pi) + hisNATe1.' ...
-                              tn 'vP.' vn '_lintr(2,pi).*time;']);
-                    end
-                    eval([tn 'vP.' vn '_lin = ' tn 'vP.' vn '_lin'';']);
-            
-                    % subtract cubic trend:
-                    eval([tn 'vP.' vn ' = ' tn 'vP.' vn '-' tn 'vP.' vn '_lin;']);
-            
-                end
-            end
-            OHC_lin = hisNATe1.OHC_lintr(1)+hisNATe1.OHC_lintr(2).*time;
-            OHC = OHC-OHC_lin;
-        end
-    end
         
     for vi=1:length(TIMESERIES)
         eval([TIMESERIES{vi} '_clim = zeros(12,1);']);
--- cfit.m
+++
@@ -1,5 +0,0 @@
-function [ylin,b] = cfit(x,y)
-xvec = [ones(length(x),1) x x.^2 x.^3];
-b = xvec\y;
-ylin = xvec*b;
-end
